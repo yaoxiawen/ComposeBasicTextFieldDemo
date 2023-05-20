b359a42b7eaa8bd65f6e4a3820a0f38a5221b720
yaoxiawen <735649296@qq.com>
2023-05-20 21:50:51 +0800

    使用TextFieldValue定位光标位置

--- app/src/main/java/com/example/composebasictextfielddemo/MainActivity.kt
+++ app/src/main/java/com/example/composebasictextfielddemo/MainActivity.kt
@@ -20,6 +20,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composebasictextfielddemo.ui.theme.ComposeBasicTextFieldDemoTheme
@@ -53,6 +55,59 @@ class MainActivity : ComponentActivity() {
                         .clickable {
                             val text = "age"
                             text1 = text
+                        })
+
+                    //使用TextFieldValue定位光标位置
+                    var text2 by remember {
+                        mutableStateOf("")
+                    }
+                    BasicTextField(
+                        value = TextFieldValue(
+                            text = text2,
+                            selection = TextRange(text2.length),
+                            composition = TextRange(0, text2.length)
+                        ),
+                        onValueChange = {
+                            if (it.text == text2) {
+                                return@BasicTextField
+                            }
+                            text2 = it.text
+                        },
+                        modifier = Modifier
+                            .padding(top = 30.dp, start = 30.dp)
+                            .width(300.dp)
+                    )
+                    Text(text = "点击输入起始默认内容age", modifier = Modifier
+                        .padding(top = 30.dp, start = 30.dp)
+                        .clickable {
+                            val text = "age"
+                            text2 = text
+                        })
+
+                    var text3 by remember {
+                        mutableStateOf(TextFieldValue())
+                    }
+                    BasicTextField(
+                        value = text3,
+                        onValueChange = {
+                            if (it.text == text3.text) {
+                                return@BasicTextField
+                            }
+                            text3 = it
+                        },
+                        modifier = Modifier
+                            .padding(top = 30.dp, start = 30.dp)
+                            .width(300.dp)
+                    )
+                    Text(text = "点击输入起始默认内容age", modifier = Modifier
+                        .padding(top = 30.dp, start = 30.dp)
+                        .clickable {
+                            val text = "age"
+                            text3 = TextFieldValue(
+                                text = text,
+                                selection = TextRange(text.length),
+                                composition = TextRange(0, text.length)
+                            )
                         })
                 }
             }
